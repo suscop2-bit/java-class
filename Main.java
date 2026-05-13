d9c0803b3a60bdc2a208dfc17e30fe80e7a0ef51
suscop2-bit <suscop2@gmail.com>
2026-05-13 01:59:33 +0100

    new code

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-public class Main {
+public class TestGit {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("=======================================");
+        System.out.println("   Java + Notepad++ + Git is WORKING!  ");
+        System.out.println("=======================================");
+        System.out.println("Next step: Push this to GitHub.");
     }
 }
